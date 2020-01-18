ef4b367c393b5305134204ed57d8d6adb0ee5680
Pedro Castro <castro.fcefyn@gmail.com>
2020-01-18 03:45:34 -0300

    Completo la clase GoogleSearchTest. Funcionando OK. El test testGooglePage esta OK

--- src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
+++ src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
@@ -1,5 +1,52 @@
 package com.qualitystream.tutorial;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleSearchTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "Z:\\Descargas\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void testGooglePage() {
+		WebElement searchBox = driver.findElement(By.name("q"));
+		
+		//Limpia cualquier texto en la caja de busqueda
+		searchBox.clear();
+		
+		//Envia al buscador el texto
+		searchBox.sendKeys("Introduccion a automation");
+		
+		//"Enter"
+		searchBox.submit();
+		
+		//Delay para que Google recupere los resultados. Esto evita un falso positivo
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		assertEquals("Introduccion a automation - Buscar con Google", driver.getTitle());
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
